75d4b69b603a34bcbfbd2cc1d14a5471c7f92511
Nursima Asilturk <nursima.asilturk99@gmail.com>
2022-07-20 00:44:08 +0300

    some designs changed

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const introHome = document.querySelector(".intro-navbar");
-introHome.style.backgroundColor = "rgb(118, 68, 138 )";
+introHome.style.backgroundColor = "rgb(138, 7, 225)";
 const introBtn = document.querySelector("#intro-btn");
 introBtn.addEventListener("click", function (e) {
   window.setTimeout(function () {
