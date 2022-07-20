236e8d64faf1af325e2dbac970ba7dab42699710
Nursima Asilturk <nursima.asilturk99@gmail.com>
2022-07-20 15:13:38 +0300

    coded first function of effects page

--- js/effect.js
+++ js/effect.js
@@ -1,2 +1,16 @@
 const effectsHome = document.querySelector("#effects-navbar");
 effectsHome.style.backgroundColor = "rgb(39, 162, 223)";
+$(document).ready(function () {
+  $("#anm-btn").click(function () {
+    $("#animate-box").animate(
+      {
+        left: "50px",
+        height: "toggle",
+      },
+      1000,
+      function () {
+        // Animation complete.
+      }
+    );
+  });
+});
